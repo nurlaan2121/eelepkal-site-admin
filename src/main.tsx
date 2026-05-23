6c94ab5efe09c9f16689a9c2fd62a4b509495030
Nurlan <nurlanabijbillaev@gmail.com>
2026-05-23 16:40:25 +0600

    test 50

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,8 @@ const queryClient = new QueryClient({
 });
 
 
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
